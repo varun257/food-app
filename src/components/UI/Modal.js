b900f8d1320eb8180e0ecae88509373c901153ee
varun257 <varun257@gmail.com>
2021-11-01 17:07:39 +0530

    feat: react food app example

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = props => {
+    return <div className={classes.Backdrop} onClick={props.onClose}></div>
+};
+const ModalOverlay = props => {
+    return <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+    </div>
+}
+
+const Modal = (props) => {
+    const portalElement = document.getElementById('overlays');
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClose={props.onClose}/>,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+            <Backdrop />
+            
+        </Fragment>
+    )
+}
+
+export default Modal;
